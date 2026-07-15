e90d00510fd224100d546c0e28dbbd812319802b
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-07-15 21:47:20 +0530

    Time: 0 ms (100%), Space: 55.3 MB (77.53%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var gcdOfOddEvenSums = function(n) {
+    // let n=nums.length;
+    // sumOdd=[]
+    // sumEven=[]
+    // for(let i=0;i<n;i++){
+    //     if(i%2==0){
+    //         num+=sumOdd
+    //     }
+    //     else
+    //        sumEven
+    // }
+
+    return n;
+};
