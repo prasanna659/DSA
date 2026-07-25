73a6ee1a260e41bc67e633bfe323941f05967f2c
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-07-26 00:10:03 +0530

    Time: 1 ms (80.85%), Space: 55.7 MB (82.98%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var maxProduct = function(n) {
+  
+    const digits = String(n).split('').map(Number).sort((a, b) => b - a);
+    
+    
+    return digits[0] * digits[1];
+};
