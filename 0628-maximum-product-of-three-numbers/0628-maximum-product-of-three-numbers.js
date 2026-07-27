22e5662100f0257f5f902e5b46c918e7e1f48ac8
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-07-27 22:21:45 +0530

    Time: 3 ms (95.5%), Space: 58.8 MB (63.96%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,11 +3,29 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    nums.sort((a, b) => a - b);
-    const n = nums.length;
+   let max1 = -Infinity, max2 = -Infinity, max3 = -Infinity;
+    let min1 = Infinity, min2 = Infinity;
 
-    const option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
-    const option2 = nums[0] * nums[1] * nums[n - 1];
+    for (const num of nums) {
+        
+        if (num > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max3 = max2;
+            max2 = num;
+        } else if (num > max3) {
+            max3 = num;
+        }
 
-    return Math.max(option1, option2);
+        if (num < min1) {
+            min2 = min1;
+            min1 = num;
+        } else if (num < min2) {
+            min2 = num;
+        }
+    }
+
+    return Math.max(max1 * max2 * max3, min1 * min2 * max1);
 };
