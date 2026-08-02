7e4efb5723d4b8b7e61f21bee302404d98bc831e
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-08-02 11:07:04 +0530

    Time: 4 ms (35.85%), Space: 59.8 MB (7.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,12 +4,11 @@
  */
 var missingNumber = function(nums) {
     let n=nums.length
-   // let set=new Set(nums)
-    nums.sort((a,b)=>a-b);
-    for(let i=0;i<n;i++){
-        if(nums[i]!==i){
-            return i
+   const set = new Set(nums);
+    for (let i = 0; i <= nums.length; i++) {
+        if (!set.has(i)) {
+            return i; 
         }
     }
-    return n
+ 
 };
