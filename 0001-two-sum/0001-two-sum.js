5850f92d2ab62bc7bfb26f36f67a4b821cc04b7f
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-08-05 11:32:59 +0530

    Time: 1 ms (88.62%), Space: 54.1 MB (85.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,12 +5,14 @@
  */
 var twoSum = function(nums, target) {
    let n = nums.length;
-
+    let map=new Map()
     for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        let complement=target-nums[i];
+
+        if(map.has(complement)){
+            return [map.get(complement),i];
+
         }
+        map.set(nums[i],i);
     }
 };
