a811c34bff3f85a6fb89a4a706e7e4fadda61d17
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-08-05 11:01:51 +0530

    Time: 28 ms (34.76%), Space: 79.2 MB (42.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,14 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let n=nums.length;
-    let freq= new Map();
-    for(let i=0;i<n;i++){
-        if(freq.has(nums[i])){
-           return true;
-        }
-        freq.set(nums[i], true);
-    }
-    return false
-    
+   return new Set(nums).size!==nums.length
 };
