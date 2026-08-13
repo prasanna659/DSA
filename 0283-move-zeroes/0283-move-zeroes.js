6211dc3deece5e4bc6d77f5554da72d5e8ce0158
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-08-13 12:44:08 +0530

    Time: 4 ms (30.54%), Space: 60.4 MB (54.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,18 +2,20 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-
 var moveZeroes = function(nums) {
-    let NonZero = 0;
+    let n=nums.length;
+    let left = 0;
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
-            
-            [nums[NonZero], nums[i]] = [nums[i], nums[NonZero]];
-            
-            
-            NonZero++;
+            nums[left] = nums[i];
+            left++;
         }
     }
+
+   
+    while (left < nums.length) {
+        nums[left] = 0;
+        left++;
+    }
 };
-    
