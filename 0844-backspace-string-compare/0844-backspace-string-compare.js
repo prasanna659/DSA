ef21131596898ddfc7eb8372e3ef7f306fa7a0c0
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-08-17 19:30:49 +0530

    Time: 2 ms (27.43%), Space: 53.4 MB (85.73%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -28,21 +28,67 @@ var backspaceCompare = function(s, t) {
 //    return false
 // };
 
-const process = (str) => {
-        const stack = [];
-        for (let char of str) {
-            if (char === '#') {
-                stack.pop(); // Removes the last character if available
+// const process = (str) => {
+//         const stack = [];
+//         for (let char of str) {
+//             if (char === '#') {
+//                 stack.pop(); // Removes the last character if available
+//             } else {
+//                 stack.push(char);
+//             }
+//         }
+//         return stack.join('');
+//     };
+
+//     return process(s) === process(t);
+
+let i = s.length - 1;
+    let j = t.length - 1;
+    let skipS = 0;
+    let skipT = 0;
+
+    while (i >= 0 || j >= 0) {
+        // Find next valid character in s
+        while (i >= 0) {
+            if (s[i] === '#') {
+                skipS++;
+                i--;
+            } else if (skipS > 0) {
+                skipS--;
+                i--;
             } else {
-                stack.push(char);
+                break;
             }
         }
-        return stack.join('');
-    };
 
-    return process(s) === process(t);
+        // Find next valid character in t
+        while (j >= 0) {
+            if (t[j] === '#') {
+                skipT++;
+                j--;
+            } else if (skipT > 0) {
+                skipT--;
+                j--;
+            } else {
+                break;
+            }
+        }
 
+        // Compare characters
+        if (i >= 0 && j >= 0 && s[i] !== t[j]) {
+            return false;
+        }
 
+        // Check if one string ended before the other
+        if ((i >= 0) !== (j >= 0)) {
+            return false;
+        }
+
+        i--;
+        j--;
+    }
+
+    return true;
 
 };
 
