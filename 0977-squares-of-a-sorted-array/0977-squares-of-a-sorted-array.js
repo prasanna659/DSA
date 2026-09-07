cb19c526fb1f1b6c8652ef978cdc9a461ab12ca7
Prasanna Kumar <kolaprasannakumar123@gmail.com>
2026-09-07 19:35:05 +0530

    Time: 7 ms (53.83%), Space: 62.4 MB (54.34%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,13 +3,8 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let n=nums.length;
-   // sqre=nums[i]**2
-    sqre=[]
-   for(let i=0;i<n;i++){
-    sqre.push(nums[i]**2);
-   }
-   sqre.sort((a,b)=>a-b)
+    const squared = nums.map(num => num * num);
     
-    return sqre
+    
+    return squared.sort((a, b) => a - b);
 };
